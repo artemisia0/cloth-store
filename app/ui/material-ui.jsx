d270cfc567ff1a22944bafd91c9ec1091aa489bd
Artem <artemisia8192@gmail.com>
2024-05-31 16:35:48 +0200

    Added products nav menu

--- app/ui/material-ui.jsx
+++ app/ui/material-ui.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import {Navbar, ThemeProvider, Input, Menu, MenuHandler, MenuList, MenuItem, Button} from '@material-tailwind/react'
+import {List, ListItem, Navbar, ThemeProvider, Input, Menu, MenuHandler, MenuList, MenuItem, Button} from '@material-tailwind/react'
 
-export {Navbar, Input, Menu, ThemeProvider, MenuHandler, MenuList, MenuItem, Button}
+export {Navbar, Input, List, ListItem, Menu, ThemeProvider, MenuHandler, MenuList, MenuItem, Button}
 
--- app/ui/productsmenu.jsx
+++ app/ui/productsmenu.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Button, Navbar, Menu, MenuHandler, MenuList, MenuItem} from "@/app/ui/material-ui"
+import {List, ListItem, Button, Navbar, Menu, MenuHandler, MenuList, MenuItem} from "@/app/ui/material-ui"
 import Link from 'next/link'
 
 
@@ -13,13 +13,64 @@ export default function ProductsMenu() {
 					</Link>
 				</Button>
 			</MenuHandler>
-			<MenuList>
-				<MenuItem>
-					Hello
-				</MenuItem>
-				<MenuItem>
-					Hi!
-				</MenuItem>
+			<MenuList className="flex flex-row justify-center align-center items-center p-1">
+				<List className="p-1">
+					<div className="flex justify-center font-bold text-sm text-blue-gray-900">
+						Men
+					</div>
+					<ListItem className="p-1">
+						<MenuItem className="p-1 flex justify-center">
+							<Link href="/products/men/shoes">
+								Shoes
+							</Link>
+						</MenuItem>
+					</ListItem>
+					<ListItem className="p-1">
+						<MenuItem className="p-1 flex justify-center">
+							<Link href="/products/men/shirts">
+								Shirts
+							</Link>
+						</MenuItem>
+					</ListItem>
+				</List>
+				<List className="p-1">
+					<div className="flex justify-center font-bold text-sm text-blue-gray-900">
+						Women
+					</div>
+					<ListItem className="p-1">
+						<MenuItem className="p-1 flex justify-center">
+							<Link href="/products/women/shoes">
+								Shoes
+							</Link>
+						</MenuItem>
+					</ListItem>
+					<ListItem className="p-1">
+						<MenuItem className="p-1 flex justify-center">
+							<Link href="/products/women/shirts">
+								Shirts
+							</Link>
+						</MenuItem>
+					</ListItem>
+				</List>
+				<List className="p-1">
+					<div className="flex justify-center font-bold text-sm text-blue-gray-900">
+						Unisex
+					</div>
+					<ListItem className="p-1">
+						<MenuItem className="p-1 flex justify-center">
+							<Link href="/products/unisex/shoes">
+								Shoes
+							</Link>
+						</MenuItem>
+					</ListItem>
+					<ListItem className="p-1">
+						<MenuItem className="p-1 flex justify-center">
+							<Link href="/products/unisex/shirts">
+								Shirts
+							</Link>
+						</MenuItem>
+					</ListItem>
+				</List>
 			</MenuList>
 		</Menu>
 	)
